adf9c31dc2903b545810b4030555084428c6da62
Jogarts95 <jogar1995@gmail.com>
2024-01-12 00:18:44 -0600

    update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,59 +8,59 @@ function Skills() {
                     Herramientas
                 </h1>
 
-                <div className="lg:flex lg:flex-row md:flex md:flex-col">
-                    <div className="text-center text-[#fff] p-2 justify-items-center align-middle w-1/3">
-                        <h1 className=' text-2xl font-semibold text-center p-3 m-3'>Frontend</h1>
-                        <div className='flex flex-row flex-wrap p-2 m-2 justify-center'>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/htmlIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                <div className="md:flex md:flex-col">
+                    <div className="md:text-center text-[#fff] p-2 justify-items-center align-middle">
+                        <h1 className=' text-2xl font-semibold text-center p-3 m-3 justify-center'>Frontend</h1>
+                        <div className='flex flex-row p-2 m-2 justify-center md:justify-center flex-wrap'>
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/htmlIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/cssIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/cssIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/jsIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/jsIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/bootstrapIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/bootstrapIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/tailwindcssIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/tailwindcssIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/reactIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/reactIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
 
                         </div>
                     </div>
 
-                    <div className="text-center text-[#fff] p-2 justify-items-center align-middle w-1/3">
+                    <div className="md:text-center text-[#fff] p-2 justify-items-center align-middle">
                         <h1 className=' text-2xl font-semibold text-center p-3 m-3'>Backend</h1>
-                        <div className='flex flex-row flex-wrap justify-center'>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/nodeIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                        <div className='flex flex-row p-2 m-2 justify-center md:justify-center flex-wrap'>
+                            <div className="md:w-[15%] h-[15%]">
+                                <img src="/nodeIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/laravelIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/laravelIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/phpIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/phpIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/mysqlIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/mysqlIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
 
                         </div>
                     </div>
 
-                    <div className="text-center text-[#fff] p-2 justify-items-center align-middle w-1/3">
+                    <div className="md:text-center text-[#fff] p-2 justify-items-center align-middle">
                         <h1 className=' text-2xl font-semibold text-center p-3 m-3'>Videojuegos</h1>
-                        <div className='flex flex-row flex-wrap justify-center'>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/csharpIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                        <div className='flex flex-row p-2 m-2 justify-center md:justify-center flex-wrap'>
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/csharpIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
-                            <div className="w-[35%] h-[35%]">
-                                <img src="/unityIcon.png" alt="" className="w-full h-full object-cover p-2 m-2" />
+                            <div className="md:w-[15%] md:h-[15%]">
+                                <img src="/unityIcon.png" alt="" className="w-[150px] p-2 m-2" />
                             </div>
 
                         </div>
